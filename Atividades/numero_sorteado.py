39e9116a1ad34fb12eb908fa82fe99d44e077e5b
Jonas Júnior <jonas.reis@shopdelivery.com.br>
2024-01-26 14:59:02 -0300

    Atualizando atividades

---
+++ Atividades/numero_sorteado.py
@@ -0,0 +1,13 @@
+#SORTEIO DE NÚMERO
+
+from random import randrange
+
+print("O número sorteado estará dentro dos números que você inserir.")
+
+valor1 = input("Digite aqui o primeiro número (Exemplo -> 1): ")
+valor2 = input("Digite aqui o segundo número: ")
+
+if valor1.isdigit() and valor2.isdigit():
+    print(f"O número sorteado foi: {randrange(int(valor1), int(valor2))}")
+else:
+    print("Insira somente números inteiros válidos")    
